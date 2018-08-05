78bf40d0d5088b7321f46eb889c7aa4d8101df95
kamildev7 <k.93kamil@gmail.com>
2018-08-05 19:41:59 +0200

    Refactored to multimodule maven project. Closes #1

---
+++ pet-clinic-web/src/main/java/spring5test/mypetclinic/MyPetClinicApplication.java
@@ -0,0 +1,12 @@
+package spring5test.mypetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPetClinicApplication.class, args);
+    }
+}
