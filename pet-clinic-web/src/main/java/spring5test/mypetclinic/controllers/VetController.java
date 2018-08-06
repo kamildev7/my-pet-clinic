560334a26c075236d2939ca12d02204ea06db961
kamildev7 <k.93kamil@gmail.com>
2018-08-06 18:33:42 +0200

    Added Vets List and updated controller. Closes #15

--- pet-clinic-web/src/main/java/spring5test/mypetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/spring5test/mypetclinic/controllers/VetController.java
@@ -1,7 +1,9 @@
 package spring5test.mypetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import spring5test.mypetclinic.services.VetService;
 
 /**
  * @author kamildev7 on 2018-08-06.
@@ -9,8 +11,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "vets/index", "/vets/index.html"})
-    public String vetList() {
+    public String vetList(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
