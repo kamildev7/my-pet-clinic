f2983111baceeff567176d50f51d18030cfcdac9
kamildev7 <k.93kamil@gmail.com>
2018-08-08 12:56:07 +0200

    Added visits, updated owner with pet relationship. Closes #26

--- pet-clinic-data/src/main/java/spring5test/mypetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/spring5test/mypetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package spring5test.mypetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author kamildev7 on 2018-08-05.
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
---
+++ pet-clinic-data/src/main/java/spring5test/mypetclinic/model/Visit.java
@@ -0,0 +1,37 @@
+package spring5test.mypetclinic.model;
+
+import java.time.LocalDate;
+
+/**
+ * @author kamildev7 on 2018-08-08.
+ */
+public class Visit extends BaseEntity {
+
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
