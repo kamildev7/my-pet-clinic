ce5a6a759b0a8608b7b652780839e17a4467d107
kamildev7 <k.93kamil@gmail.com>
2018-08-12 19:37:50 +0200

    Added Visit Entity. Closes #38

--- pet-clinic-data/src/main/java/spring5test/mypetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/spring5test/mypetclinic/model/Visit.java
@@ -1,14 +1,23 @@
 package spring5test.mypetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * @author kamildev7 on 2018-08-08.
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
